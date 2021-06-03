88583a669f9c97d0998b196516df022f0f2bc701
ashishgajjar <rentkent29@gmail.com>
2021-06-04 00:07:16 +0530

    Authentication completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,19 +4,33 @@ const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get("Cookie").split(';')[0].trim().split('=')[1];
-  console.log(req.session);
+  let message = req.flash('error');
+  if(message.length >0){
+    message = message[0];
+  }else{
+    message = null;
+  }
+  // console.log(req.session);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
     isAuthenticated: false,
+    errorMessage:message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if(message.length >0){
+    message = message[0];
+  }else{
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
     isAuthenticated: false,
+    errorMessage:message,
   });
 };
 
@@ -26,6 +40,7 @@ exports.postLogin = (req, res, next) => {
 
   User.findOne({ email: email }).then((user) => {
     if (!user) {
+      req.flash('error','Invalid email or password');
       return res.redirect("/login");
     }
 
@@ -40,6 +55,7 @@ exports.postLogin = (req, res, next) => {
             res.redirect("/");
           });
         }
+        req.flash('error','Invalid email or password');
         res.redirect("/login");
       })
       .catch((err) => {
@@ -64,10 +80,8 @@ exports.postSignup = (req, res, next) => {
     .then((userDoc) => {
       if (userDoc) {
         // return console.log('Use already exist');
-        return sweetAlert.fire({
-          title: "User exist",
-        });
-        // res.redirect('/signup');
+        req.flash('error','Email already exist');
+       return  res.redirect('/signup');
       }
       return bcrypt
         .hash(password, 12)
