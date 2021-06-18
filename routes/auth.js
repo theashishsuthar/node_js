6740eee46556cf874611db7f0031797543e96c9d
theashishsuthar <sutharashish5656@gmail.com>
2021-06-18 16:44:43 +0530

    Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const {check}  = require('express-validator/check')
 const authController = require('../controllers/auth');
 
 const router = express.Router();
@@ -12,7 +12,7 @@ router.post('/login', authController.postLogin);
 
 router.post('/logout',authController.postLogout);
 
-router.post('/signup',authController.postSignup)
+router.post('/signup',check('email').isEmail() ,authController.postSignup)
 
 router.get('/reset',authController.getReset);
 
